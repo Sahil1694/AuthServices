47301c0871a5e950e2e2b66ce65ac1362962ea44
Sahil Khilari <sahilkhilari16@gmail.com>
2024-12-23 16:20:07 +0530

    initial commir

---
+++ src/main/java/entities/UserInfo.java
@@ -0,0 +1,37 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class UserInfo {
+
+    @Id
+    @Column
+    private String userId;
+
+    private String username;
+
+    private String password;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+
+    private Set<UserRole> roles = new HashSet<>();
+
+
+}
---
+++ src/main/java/service/CustomUserDetails.java
@@ -0,0 +1,8 @@
+package service;
+
+import entities.UserInfo;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails extends UserInfo, UserDetails {
+
+}
