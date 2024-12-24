30f47574ba925e6b00d37fe3cf842230dd82acf5
Sahil Khilari <sahilkhilari16@gmail.com>
2024-12-25 00:44:58 +0530

    Changes Fixed

--- src/main/java/service/CustomUserDetails.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import entities.UserInfo;
-import org.springframework.security.core.userdetails.UserDetails;
-
-//public class CustomUserDetails extends UserInfo, UserDetails {
-//
-//}
